ccede79579f0b1a6a16f70d73ec3b9eb9fd7dd1f
Lancy Norbert Fernandes <>
2019-03-19 20:13:32 +1300

    Added DrawView which basically is responsible for drawing the content on the screen.

---
+++ app/src/main/java/com/example/drawingapplication/DrawView.java
@@ -0,0 +1,30 @@
+package com.example.drawingapplication;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DrawView extends View {
+
+    public DrawView(Context context) {
+        super(context);
+    }
+
+    public DrawView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DrawView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public DrawView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
